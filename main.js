e978907e22dc1016a764db8c1dc915cb93f43fd3
phenix1229 <paul.garay@codeimmersives.com>
2019-12-05 19:24:57 -0500

    done

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const Ray = () => {
 
   length: 0,
 
-  
+  push: function(value){
+    this[this.length] = value;
+    this.length++;
+  },
+
+  pop: function(){
+    const lastValue = this[this.length - 1];
+    delete this[this.length -1];
+    this.length--;
+    return lastValue;
+  },
+
+  unshift: function(value){
+    for (i = this.length; i > 0; i--){
+      this[i] = this[i - 1];
+    }
+    this.length++
+    this[0] = value;
+  },
+
+  shift: function(){
+    const firstValue = this[0];
+    delete this[0];
+    for (i = 0; i < this.length; i++){
+      this[i] = this[i + 1];
+    }
+    this.length--;
+    return firstValue;
+  },
 }
 }
 
